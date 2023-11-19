30b293269d43b01152955802618e9f4eec4d3c33
GohYuJin <yujin_g1995@hotmail.com>
2023-11-19 23:43:31 +0800

    updated profile picture links

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -31,7 +31,7 @@ export const recommendationData: recommendation[] = [
               His invaluable contributions to my PhD Studies have not only resolved technical roadblocks \
               but have also enriched my understanding of Python programming and machine learning. Without a doubt, \
               Yujin is an asset to any team and an individual whose commitment to excellence is truly commendable.",
-    image: "https://media.licdn.com/dms/image/C5603AQE0QLUXijb--w/profile-displayphoto-shrink_100_100/0/1606118614865?e=1698883200&v=beta&t=KLUBvw0twTMJiYh1RiH1y3IwgQIDgKgxrbgM2HEEbSo",
+    image: "https://media.licdn.com/dms/image/C5603AQE0QLUXijb--w/profile-displayphoto-shrink_100_100/0/1606118615391?e=1706140800&v=beta&t=Kosl-0231wqlqh8a5GnmY8bibKnonyramOizPxe0RXQ",
     link: "https://www.linkedin.com/in/yu-jin-goh-737939143/details/recommendations/",
   },
   {
@@ -44,7 +44,7 @@ export const recommendationData: recommendation[] = [
               Yu Jin is also highly knowledgable about domains on Artificial intelligence and Cyber security \
               and the tools used in them. \
               Yu Jin is a pleasant individual to work with and I highly recommend him for future job prospects.",
-    image: "https://media.licdn.com/dms/image/D5603AQEI0CfWFP_hyA/profile-displayphoto-shrink_100_100/0/1680244704807?e=1698278400&v=beta&t=HUd0Vd3QhB8ZiaxPB1YYi45sw-1-hhrFKL1cS8Xz8FM",
+    image: "https://media.licdn.com/dms/image/D5603AQEI0CfWFP_hyA/profile-displayphoto-shrink_100_100/0/1680244706439?e=1706140800&v=beta&t=9mesfvxKrx_nFJILYDagvmDfppA7-fdcoQ-Eq5DFFRQ",
     link: "https://www.linkedin.com/in/yu-jin-goh-737939143/details/recommendations/",
   },
   {
@@ -54,10 +54,10 @@ export const recommendationData: recommendation[] = [
     review: "Yu Jin is one of the most outstanding, talented individuals I have come across. \
               He's an extremely fast learner - having mastered java and python before commencing his studies at SUTD,\
               he served the role of a teaching assistant under our common mentor, where he taught \
-              Python, Tableau, Java, WEKA and R to a class of Master students. He's one of the rare people you’d come\
+              Python, Tableau, Java, WEKA and R to a class of Master students. He's one of the rare people you d come\
               across that simply excels at every thing he does. \
               Someone you can definitely rely on - an asset to any team!",
-    image: "https://media.licdn.com/dms/image/C4D03AQHPIluWwlCnJQ/profile-displayphoto-shrink_100_100/0/1655404958714?e=1698278400&v=beta&t=89aYc1zEfLeq2duyngMfCCzrAK7MRyPgGgDcPPSrdls",
+    image: "https://media.licdn.com/dms/image/C4D03AQHPIluWwlCnJQ/profile-displayphoto-shrink_100_100/0/1655404960239?e=1706140800&v=beta&t=XaLwZcJF0lDCAlMVXszsRTMM-6V0T8Jm3jGMUGpPVrc",
     link: "https://www.linkedin.com/in/yu-jin-goh-737939143/details/recommendations/",
   },
 ];
