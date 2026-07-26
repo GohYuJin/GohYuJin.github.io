e6439201e855430b41b27b257751e771dd3b5331
GohYuJin <yujin_g1995@hotmail.com>
2026-07-26 11:47:26 -0700

    updated job title

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -38,7 +38,7 @@ export default function LandingPage() {
           Goh Yu Jin
         </Typography>
         <Typography variant="h5" margin="auto" component="h2">
-          Computer Vision Research Engineer
+          Machine Learning Engineer
         </Typography>
       </section>
 
